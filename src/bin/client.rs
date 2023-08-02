ee5adc4343a33336d5b3e307f82cbd2d4addd106
Ben Civjan <bcivjan@amazon.com>
2023-08-01 17:40:18 -0700

    Perform key exchange over tcp

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -3,9 +3,12 @@ use aws_lc_rs::{
     kem::{KemPrivateKey, KemPublicKey, KYBER768_R3},
 };
 use clap::Parser;
+use std::io::{Read, Write};
 use std::net::TcpStream;
-use std::io::Read;
 use std::str;
+
+const KYBER768_R3_CIPHERTEXT_LENGTH: usize = 1088;
+const KYBER768_R3_PUBLIC_KEY_LENGTH: usize = 1184;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
@@ -14,37 +17,43 @@ struct Args {
     #[arg(short, long)]
     address: String,
     /// Server port
-    #[arg(short, long, default_value_t = 443)]
+    #[arg(short, long, default_value_t = 8000)]
     port: u64,
 }
 
 // TODO: Create server and client exchange using kyber + hybrid?
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     println!("Address: {}", args.address);
 
-    match TcpStream::connect(format!("{}:8000", args.address)) {
-        Ok(mut stream) => {
-            println!("Successfully connected to the server!");
-            let mut buffer = [0u8; 11];
+    let mut stream = TcpStream::connect(format!("{}:8000", args.address))?;
 
-            // read up to 10 bytes
-            let _n = stream.read(&mut buffer[..]).unwrap();
-            println!("n: {:?}", str::from_utf8(&buffer).unwrap());
-        }
-        Err(e) => {
-            eprintln!("Error: {}", e);
-        }
-    }
+    println!("Successfully connected to the server!");
+
+    let (priv_key, public_key) = kyber_keygen()?;
+
+    let mut buffer = [0u8; KYBER768_R3_PUBLIC_KEY_LENGTH];
+    let mut ct_buf = [0u8; KYBER768_R3_CIPHERTEXT_LENGTH];
+
+    buffer.copy_from_slice(public_key.as_ref());
+    println!("{:x?}", buffer);
+
+    let _pk_num_bytes = stream.write(&buffer)?;
+
+    let _ct_num_bytes = stream.read(&mut ct_buf)?;
+
+    let shared_secret = kyber_decaps(priv_key, &mut ct_buf)?;
+
+    println!("Shared secret: {:x?}", shared_secret);
+    Ok(())
 }
 
-fn kyber_keygen() -> Result<KemPublicKey, Unspecified> {
+fn kyber_keygen() -> Result<(KemPrivateKey, KemPublicKey), Unspecified> {
     let priv_key = KemPrivateKey::generate(&KYBER768_R3)?;
-    priv_key.compute_public_key()
+    let pub_key = priv_key.compute_public_key()?;
+    Ok((priv_key, pub_key))
 }
 
-fn kyber_decaps(priv_key: KemPrivateKey, ciphertext: &mut [u8]) {
-    let result = priv_key.decapsulate(ciphertext, Unspecified, |ss| {
-        Ok(ss.to_vec())
-    });
+fn kyber_decaps(priv_key: KemPrivateKey, ciphertext: &mut [u8]) -> Result<Box<[u8]>, Unspecified> {
+    priv_key.decapsulate(ciphertext, Unspecified, |ss| Ok(ss.into()))
 }
--- src/bin/daemon.rs
+++ src/bin/daemon.rs
@@ -3,27 +3,44 @@ use aws_lc_rs::{
     kem::{KemPrivateKey, KemPublicKey, KYBER768_R3},
 };
 use clap::Parser;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 use std::thread;
-use std::io::Write;
 
-fn main() {
+const KYBER768_R3_PUBLIC_KEY_LENGTH: usize = 1184;
+
+fn main() -> Result<(), std::io::Error> {
     println!("I am the Daemon!");
 
-    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
     println!("listening started, ready to accept");
+
     for stream in listener.incoming() {
-        thread::spawn(|| {
-            let mut stream = stream.unwrap();
-            stream.write(b"Hello World\r\n").unwrap();
+        thread::spawn(|| -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+            let mut stream = stream?;
+
             println!("Connection established");
+            let mut buffer = [0u8; KYBER768_R3_PUBLIC_KEY_LENGTH];
+
+            println!(
+                "Number of public key bytes read: {}",
+                stream.read(&mut buffer[..])?
+            );
+            println!("Received public key: {:x?}", buffer);
+
+            let (ciphertext, shared_secret) = kyber_encaps(&buffer)?;
+            let _ct_num_bytes = stream.write(&ciphertext)?;
+
+            println!();
+            println!("Shared secret: {:x?}", shared_secret);
+            Ok(())
         });
     }
+
+    Ok(())
 }
 
-fn kyber_encaps(public_key_bytes: &[u8]) {
-    let public_key = KemPublicKey::new(&KYBER768_R3, public_key_bytes).unwrap();
-    let result = public_key.encapsulate(Unspecified, |ct, ss| {
-        Ok((ct.to_vec(), ss.to_vec()))
-    });
+fn kyber_encaps(public_key_bytes: &[u8]) -> Result<(Box<[u8]>, Box<[u8]>), Unspecified> {
+    let public_key = KemPublicKey::new(&KYBER768_R3, public_key_bytes)?;
+    public_key.encapsulate(Unspecified, |ct, ss| Ok((ct.into(), ss.into())))
 }
